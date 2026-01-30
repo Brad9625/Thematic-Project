54efb21d892c415b49fe05267a5ee4cc001dd61d
Arran McDougle <arran.mcdougle@gmail.com>
2026-01-30 10:24:16 +0000

    slight progress on websocket movement

--- Flask App/app.py
+++ Flask App/app.py
@@ -3,19 +3,31 @@ from flask_socketio import SocketIO, send, emit, join_room, leave_room
 from map import Node, Location, Map
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 #page loading
 @app.route('/')
 def index_page():
     return render_template('index.html')
 
+@app.route('/client')
+def client_page():
+    return render_template('TEMP.html')
+
 #websocket handlers
 @socketio.on('move')
 def makemove(data):
-    detective = data[0]
-    locationTo = data[1]
-    method = data[2]
-    emit("detMove", ,broadcast=True)  #TODO - validate and modify transport cards    
+    detective = data['detective']
+    locationTo = data['locationTo']
+    method = data['method']
+    emit(
+        'detMove',
+        {
+            'detective': detective,
+            'locationTo': locationTo
+        },
+        broadcast=True
+    )  #TODO - validate and modify transport cards    
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
