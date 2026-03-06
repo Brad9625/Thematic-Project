5a89761b8e39ecf8b78cc9b234c4c7c9b0df5018
aceee98 <dillanj578@gmail.com>
2026-03-06 19:50:36 +0000

    Edited file structure

---
+++ Deliverable Product/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/rules")
+def rules():
+    return render_template("rules.html")
+
+@app.route("/play")
+def play():
+    return render_template("GameScreen.html")
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
